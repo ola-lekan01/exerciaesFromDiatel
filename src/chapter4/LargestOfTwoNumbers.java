f4890ece8da79c348c733a9cbff0e03409852354
Ola Lekan <lekan.sofuyi01@gmail.com>
2022-07-01 14:44:38 +0100

    Additional LoopTesting Exercises update.

--- src/chapter4/LargestOfTwoNumbers.java
+++ src/chapter4/LargestOfTwoNumbers.java
@@ -1,2 +1,33 @@
-package chapter4;public class LargestOfTwoNumbers {
+// TODO: 6/30/2022  Please kindly look through again for update
+
+package chapter4;
+
+import java.util.Scanner;
+
+public class LargestOfTwoNumbers {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int number = 0;
+        int number2 = 0;
+        int counter = 1;
+        int firstLargest = -500;
+        int secondLargest = 0;
+
+        while (counter <= 5) {
+            System.out.println("Please Enter your number: ");
+            number = input.nextInt();
+
+            if (number > firstLargest) {
+                firstLargest = number;
+            }
+            if( firstLargest < secondLargest)
+                secondLargest = number2;
+
+            counter++;
+        }
+        System.out.printf("The Largest of two numbers is %d %d", firstLargest , secondLargest);
+    }
 }
--- src/chapter4/LoopTesting.java
+++ src/chapter4/LoopTesting.java
@@ -4,6 +4,11 @@ import java.util.Scanner;
 
 public class LoopTesting {
     public static void main(String[] args) {
+/*
+        double principal = 1000.0;
+        double rate = 0.05;
+        double amount = 0.00;
+
 
         System.out.println("Using The For Loop");
         for(int counter = 1 ; counter <= 100 ; counter+=8 ){
@@ -30,5 +35,93 @@ public class LoopTesting {
         System.out.println( "The Sum of all grade entered is: " + total);
         System.out.println( "The Average sum of all grade entered is: " + total / 10);
 
+        System.out.printf("%s%20s%n" , "year" , "Amount on Deposit" );
+
+        for(int year = 1 ; year <=10 ; ++year){
+            amount = principal * Math.pow(1 + rate , year);
+
+            System.out.printf("%4d%20.2f%n" , year , amount);
+        }
+
+        int counter = 0;
+        int total = 0;
+        int aCount = 0;
+        int bCount = 0;
+        int cCount = 0;
+        int dCount = 0;
+        int eCount = 0;
+        int fCount = 0;
+
+        Scanner input = new Scanner(System.in);
+        System.out.printf("%s%n%s%n%s%n%s%n" ,"Enter Scores between 0 and 100 " , "Enter End of File After Entering all the scores Correctly" ,
+                "For Windows Press Control Z and Enter to signify End of File" , "For Apple Press CMD z and Enter to Signify End of Data Entry");
+        while (input.hasNext()) {
+            int grade = input.nextInt();
+            total += grade;
+            counter++;
+
+            switch (total / 10) {
+                case 10: //Who scores above 90
+                case 9:
+                    ++aCount;
+                    break;
+                case 8:
+                    ++bCount;
+                    break;
+                case 7:
+                    ++cCount;
+                    break;
+                case 6:
+                    ++dCount;
+                    break;
+                case 5:
+                    ++eCount;
+                    break;
+                default:
+                    ++fCount;
+                    break;
+            }
+        }
+                System.out.printf("%nGrade Report: %n");
+
+        if (counter != 0) {
+            double average = (double) total / counter;
+
+            System.out.printf("Total of %d student score Calculated is %d%n", counter, total);
+            System.out.printf("The Average total is: %.2f%n", average);
+            System.out.printf("n%s%n%s%d%n%s%d%n%s%d%n%s%d%n%s%d%n", "Number of Students who received each grade" ,
+                    " A: ", aCount,
+                    " B: ", bCount,
+                    " C: ", cCount,
+                    " D: ", dCount,
+                    " E: ", eCount,
+                    " F ", fCount);
+            }
+        else {
+            System.out.println("You have not entered any Score, Please Try again, THANKS");
+        }
+*/
+        Scanner input = new Scanner(System.in);
+        int counter1 =1;
+        int number = 1;
+
+        System.out.println("Enter the base length: ");
+        int rows = input.nextInt();
+
+        while (counter1 < rows){
+
+            int number2 = 0;
+            int counter2 = 1;
+
+            while (counter2 < counter1){
+
+                System.out.printf( "%d" , number + number2);
+                number2++;
+                counter2++;
+            }
+
+            System.out.println();
+            counter1++;
+        }
     }
 }
