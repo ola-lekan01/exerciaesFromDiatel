e5147edf776e1825ea83bfda8f4f56e2b95c8e76
Ola Lekan <lekan.sofuyi01@gmail.com>
2022-07-03 00:05:11 +0100

    Initial Commit for Table of Squares and Cubes Without Loops

---
+++ src/chapter2/TableOfSquaresAndCubes.java
@@ -0,0 +1,27 @@
+package chapter2;
+
+public class TableOfSquaresAndCubes {
+
+    public static void main(String[] args) {
+
+        int number = 0;
+        int number1 = 1;
+        int number2 = 2;
+        int number3 = 3;
+        int number4 = 4;
+        int number5 = 5;
+        int number6 = 6;
+        int number7 = 7;
+        int number8 = 8;
+        int number9 = 9;
+        int number10 = 10;
+
+        System.out.printf("%s%10s%10s%n" , "Number" , "Square" , "Cubes");
+        System.out.printf("%d%10d%10d%n" , number , number * number , number * number *number);
+        System.out.printf("%d%10d%10d%n" , number1 , number1 * number1 , number1 * number1 *number1);
+        System.out.printf("%d%10d%10d%n" , number2 , number2 * number2 , number2 * number2 *number2);
+        System.out.printf("%d%10d%10d%n" , number3 , number3 * number3 , number3 * number3 *number3);
+
+
+    }
+}
