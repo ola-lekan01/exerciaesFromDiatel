80e021ad9268c464040049aa2537fa18e65822e8
Ola Lekan <lekan.sofuyi01@gmail.com>
2022-08-04 10:42:31 +0100

    Updated commits on Seven Segment Display.

---
+++ src/Chapter7/SevenSegmentDisplay/SevenSegmentDisplay.java
@@ -0,0 +1,14 @@
+package Chapter7.SevenSegmentDisplay;
+
+import java.time.LocalDate;
+
+public class SevenSegmentDisplay {
+
+    int [] [] displays = new int [4][5];
+
+    public void setScreen(){
+
+    }
+
+}
+
