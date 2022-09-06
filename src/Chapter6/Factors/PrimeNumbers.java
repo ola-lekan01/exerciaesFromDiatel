ad6d3f4ad67eaf8e6f5d1f5e273f4847c1f7717f
Ola Lekan <lekan.sofuyi01@gmail.com>
2022-09-06 15:48:38 +0100

    Update commits on Prime Numbers.

---
+++ src/Chapter6/Factors/PrimeNumbers.java
@@ -0,0 +1,29 @@
+package Chapter6.Factors;
+
+public class PrimeNumbers {
+    //Don't Do This
+    public static boolean calculateFactors(int number) {
+        int factors = 0;
+        boolean isPrime = true;
+        for (int count = 1; count <= number; count++) {
+            if(number % count == 0) {
+                factors++;
+                if(factors > 2) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+        return isPrime;
+    }
+    //Do This
+    public static boolean isPrimed(int number){
+        int [] factors = {2,3,4,5,6,7,9};
+        if(number == factors[0] || number == factors[1] ||
+                number == factors[3] || number == factors[5]) return true;
+        for (int factor : factors){
+            if(number % factor == 0) return false;
+        }
+        return true;
+    }
+}
